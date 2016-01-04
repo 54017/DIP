601f20dce411877082d287c63e92a3c9d0442dde
54017 <571738739@qq.com>
2016-01-04 14:56:35 +0800

    finish text float

--- explode/js/index.js
+++ explode/js/index.js
@@ -1,8 +1,7 @@
 (function() {
 
-	"use strict";
 
-	var stage, circles, rawData, windowSize = 6, offsetLeft, offsetTop;
+	var stage, circles, rawData, offsetLeft, offsetTop, textStage, colors = ["rgb(52, 217, 244), rgb(255, 224, 40), rgb(80, 255, 38), rgb(253, 0, 32)"];
 
 	var changeToMatrix = function(rawData, height, width) {
 		//转化为常见的矩阵形式（RGBA矩阵每行是4*width个）
@@ -16,25 +15,9 @@
 	}
 
 	//像素点化
-	var pixelate = function() {
-		var image = document.getElementById('logo');
-		//将原图画在canvas上
-		var canvas = document.getElementById('logo-canvas');
-		try {
-			var context = canvas.getContext('2d');
-		} catch(e) {
-			console.log(e)
-			alert("你的浏览器版本太低啦，请使用Chrome，Safari，火狐或者IE10及其以上的浏览器");
-			return;
-		}
-		var height = image.height,
-			width = image.width;
+	var pixelate = function(context, height, width, state) {
 		offsetLeft = (window.innerWidth - width)/2;
 		offsetTop = (window.innerHeight - height)/2;
-		console.log(offsetLeft)
-		canvas.setAttribute('width', width);
-		canvas.setAttribute('height', height);
-		context.drawImage(image, 0, 0, width, height);
 		try {
 			rawData = context.getImageData(0, 0, width, height).data;
 		} catch(e) {
@@ -73,8 +56,8 @@
 	            	r = parseInt(resultMatrix[i][4 * j]),
 	            	g = parseInt(resultMatrix[i][4 * j + 1]),
 	            	b = parseInt(resultMatrix[i][4 * j + 2]),
-	            	color = "rgba(" + r + ", " + g + ", " + b + ", " + 1 + ")";
-	            if (resultMatrix[i][4 * j + 3] == 0 || (r < 120 && g < 120 && b < 120)) {
+	            	color = "rgb(" + r + ", " + g + ", " + b + ")";
+	            if (resultMatrix[i][4 * j + 3] == 0 || (r < 200 && g < 200 && b < 200)) {
 	            	continue;
 	            }
 	         	circle.radius = r;
@@ -83,10 +66,10 @@
 	            circle.y = Math.random() * window.innerHeight;
 	            circle.originX = originX + offsetLeft;
 	            circle.originY = originY + offsetTop;
-	            console.log(originX, originY)
+	            circle.alpha = 0.2 + Math.random()*0.5;
 	            stage.addChild(circle);
 	            circles.push(circle);
-	            tweenCircle(circle, 'float')
+	            tweenCircle(circle, state)
 			}
     	}
 	}
@@ -95,13 +78,20 @@
 		if(c.tween) c.tween.kill();
 		if (state == 'in') {
 			c.tween = TweenLite.to(c, 0.4, {x: c.originX, y: c.originY, ease:Quad.easeInOut, alpha: 1, radius: 5, scaleX: 0.4, scaleY: 0.4, onComplete: function() {
-                c.movement = 'jiggle';
-                tweenCircle(c);
+				tweenCircle(c, 'jiggle');
             }});
 		} else if (state == 'float') {
-			c.tween = TweenLite.to(c, 5 + Math.random()*3.5, {x: c.x + -100+Math.random()*200, y: c.y + -100+Math.random()*200, ease:Quad.easeInOut, alpha: 0.2 + Math.random()*0.5,
+			c.tween = TweenLite.to(c, 5 + Math.random()*3.5, {x: c.x + -100+Math.random()*200, y: c.y + -100+Math.random()*200, ease:Quad.easeInOut, alpha: 0.2 + Math.random()*0.5, onComplete: function() {
+                tweenCircle(c, 'float');
+            }});
+		} else if (state == 'out') {
+			c.tween = TweenLite.to(c, 0.8, {x: window.innerWidth*Math.random(), y: window.innerHeight*Math.random(), ease:Quad.easeInOut, alpha: 0.2 + Math.random()*0.5, scaleX: 1, scaleY: 1, onComplete: function() {
+                tweenCircle(c, 'float');
+            }});
+		} else if (state == 'jiggle') {
+			c.tween = TweenLite.to(c, 0.05, {x: c.originX + Math.random()*3, y: c.originY + Math.random()*3, ease:Quad.easeInOut,
                     onComplete: function() {
-                        tweenCircle(c, 'float');
+                        tweenCircle(c, 'jiggle');
                     }});
 		}
 	}
@@ -111,42 +101,86 @@
         requestAnimationFrame(animate);
     }
 
-    var initCircles = function() {
-    	circles = []
-    	for(var i = 0; i < 300; ++i) {
-			var circle = new createjs.Shape(),
-            	radius = Math.sqrt(windowSize * windowSize) / 2,
-            	x = window.innerWidth*Math.random(),
-            	y = window.innerHeight*Math.random(),
-            	r = parseInt(resultMatrix[i][4 * j]),
-            	g = parseInt(resultMatrix[i][4 * j + 1]),
-            	b = parseInt(resultMatrix[i][4 * j + 2]),
-            	color = "rgba(" + r + ", " + g + ", " + b + ", " + 1 + ")";
-            if (resultMatrix[i][4 * j + 3] == 0 || (r < 120 && g < 120 && b < 120)) {
-            	continue;
-            }
-         	circle.radius = r;
-            circle.graphics.beginFill(color).drawCircle(0, 0, radius);
-            circle.x = y;
-            circle.y = x;
-            stage.addChild(circle);
-            circls.push(circle);
+    
+
+    var explode = function() {
+    	for (var i = 0; i < circles.length; ++i) {
+    		tweenCircle(circles[i], 'out');
     	}
+    	setTimeout(function() {
+    		formText("Final News");
+    	}, 1000);
+    }
+
+    var formText = function(words) {
+    	var text = new createjs.Text(words, "50px 'Source Sans Pro'", "green");
+        textStage.addChild(text);
+        textStage.update();
+        var count = 0;
+        var canvas = document.getElementById('text'),
+        	context = canvas.getContext('2d');
+        var width = canvas.width,
+        	height = canvas.height;
+        var pix = context.getImageData(0, 0, width, height).data;
+       	var matrixData = changeToMatrix(pix, height, width);
+        var textPixels = [];
+        var offsetX = (window.innerWidth - width)/2,
+			offsetY = (window.innerHeight - height)/2;
+		var windowSize = 4, space = windowSize / 2;
+		for(var i = 0; i < height - windowSize; i += windowSize) {
+			for (j = 0; j < width - windowSize; j += windowSize) {
+	            var	radius = Math.sqrt(windowSize * windowSize) / 2,
+	            	originY = i + space,
+	            	originX = j + space,
+	            	r = parseInt(matrixData[i][4 * j]),
+	            	g = parseInt(matrixData[i][4 * j + 1]),
+	            	b = parseInt(matrixData[i][4 * j + 2]),
+	            	color = colors[Math.floor(i%colors.length)];
+	            if (matrixData[i][4 * j + 3] == 0) {
+	            	continue;
+	            }
+	            ++count;
+	         	circles[count].radius = r;
+	            circles[count].graphics.beginFill(color);
+	            
+	            circles[count].originX = originX + offsetLeft;
+	            circles[count].originY = originY + offsetTop;
+	            tweenCircle(circles[count], 'in')
+			}
+    	}
+      
     }
 
 	var init = function() {
 		stage = new createjs.Stage('stage');
 		stage.canvas.width = window.innerWidth;
 		stage.canvas.height = window.innerHeight;
+		textStage = new createjs.Stage('text');
+		var image = document.getElementById('logo');
+		//将原图画在canvas上
+		var canvas = document.getElementById('logo-canvas'),
+			height = image.height,
+			width = image.width;
+		canvas.width = width;
+		canvas.height = height;
+		try {
+			var context = canvas.getContext('2d');
+		} catch(e) {
+			console.log(e)
+			alert("你的浏览器版本太低啦，请使用Chrome，Safari，火狐或者IE10及其以上的浏览器");
+			return;
+		}	
+		context.drawImage(image, 0, 0, width, height);
+		pixelate(context, height, width, 'float');
+		animate();
 		setTimeout(function() {
-			pixelate();
-			animate();
-		}, 100)
-		setTimeout(function(){
 			for (var i = 0; i < circles.length; ++i) {
 				tweenCircle(circles[i], 'in')
 			}
-		}, 1000)
+		}, 1500)
+		setTimeout(function() {
+			explode();
+		}, 3000)
 	}
 
 	window.onload = init;
